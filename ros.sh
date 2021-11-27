1eedff70d31b446222626ebee0a9f2c7472ee851
KevinUrrutia <kurru006@ucr.edu>
2021-11-27 14:54:26 -0800

    created ros script

---
+++ ros.sh
@@ -0,0 +1,25 @@
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+magenta=`tput setaf 5`
+cyan=`tput setaf 6`
+reset=`tput sgr0`
+
+echo "${green}Setting up sources list"
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+echo "${green}Setting up your keys"
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
+sudo apt-get update
+
+echo "${green}Installing ROS melodic full"
+sudo apt-get install ros-melodic-desktop-full
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo rosdep init
+rosdep update
+
+echo "${green}Finished installing ROS"
